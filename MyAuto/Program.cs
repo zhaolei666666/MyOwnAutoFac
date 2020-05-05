5d426338dbc06bf8078889a3837c107d46fea573
util <2691431903@qq.com>
2020-05-05 23:16:44 +0800

    add dll

--- MyAuto/Program.cs
+++ MyAuto/Program.cs
@@ -10,6 +10,7 @@ namespace MyAuto
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
---
+++ MyAutoImplement/V1/MyContainor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAutoImplement.V1
+{
+    public class MyContainor
+    {
+        public void RegisterType<T>() where T : class, new()
+        {
+            Type t = typeof(T);
+
+            var obj = Activator.CreateInstance(t);
+        }
+
+    }
+}
